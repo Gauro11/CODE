4e7e8ebfb1a1b7b61e3db970f7f2347fe33c1efc
Gauro11 <milbertgaringa5@gmail.com>
2025-11-10 17:59:24 -0800

    we

--- connection.php
+++ connection.php
@@ -1,22 +1,22 @@
 <?php
-// $host = 'localhost';
-// $db = 'alazima';
-// $user = 'root';
-// $pass = '';
+$host = 'localhost';
+$db = 'alazima';
+$user = 'root';
+$pass = '';
 
-// $conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db);
 
-// if ($conn->connect_error) {
-//     die(json_encode(['success' => false, 'message' => 'DB Connection failed: ' . $conn->connect_error]));
-// }
-
-$servername = "localhost";
-$username = "u665838367_alazimaa";
-$password = '6$HvZ#Vd'; // safer
-
-$dbname = "u665838367_alazima";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die(json_encode(['success' => false, 'message' => 'DB Connection failed: ' . $conn->connect_error]));
 }
+
+// $servername = "localhost";
+// $username = "u665838367_alazimaa";
+// $password = '6$HvZ#Vd'; // safer
+
+// $dbname = "u665838367_alazima";
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
